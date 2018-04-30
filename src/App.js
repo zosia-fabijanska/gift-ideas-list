edb06f16b042808c0bbabc88c3a38320a0542bfb
Patrick Stockwell <stockwellpatrick@gmail.com>
2018-04-30 21:57:59 +1000

    Patrick's initial commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          Here is the second line.
         </p>
       </div>
     );
